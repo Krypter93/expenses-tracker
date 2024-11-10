736aaa308f06860e30c27d91bbd9952f641fd6b7
Ahmed <ahmedfdez93@gmail.com>
2024-11-10 10:29:55 -0500

    Logic of the table to show the data

--- src/pages/DataTable.tsx
+++ src/pages/DataTable.tsx
@@ -1,7 +1,13 @@
+import { RootState } from "../redux/store"
+
 export const DataTable = () => {
+    const dataState = JSON.parse(localStorage.getItem('expenses') || '[]')
+    console.log(dataState);
+    
     return (
         <>
-            <table className="table-auto">
+        <section className="w-4/5 bg-gray-200 mt-7 mx-auto font-mono flex justify-center items-center">
+        <table className="table-auto">
                 <thead>
                     <tr>
                         <th>Expense Name</th>
@@ -10,9 +16,16 @@ export const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    {dataState && dataState.map((item: RootState, index: number) => (
+                        <tr key={index}>
+                            <td>{item.title}</td>
+                            <td>{item.category}</td>
+                            <td>{item.amount}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+        </section>  
         </>
     )
 }
