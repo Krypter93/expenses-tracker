c8b754263bea461cc36183a177c1dd0ebf31c265
Ahmed <ahmedfdez93@gmail.com>
2024-11-11 16:35:52 -0500

    More media queries for the data table

--- src/pages/DataTable.tsx
+++ src/pages/DataTable.tsx
@@ -7,27 +7,27 @@ export const DataTable = () => {
     
     return (
         <>
-        <section className="w-4/5 bg-gray-200 mt-7 mx-auto font-mono flex justify-between rounded">
+        <section className="w-4/2 md:w-4/5 bg-gray-200 mt-16 mx-auto font-mono flex justify-between rounded">
         <table className="w-full text-center border-solid border-2 border-slate-400">
                 <thead>
                     <tr>
-                        <th className="bg-green-700 text-white">Expense Name</th>
-                        <th className="bg-green-700 text-white">Category</th>
-                        <th className="bg-green-700 text-white">Amount</th>
+                        <th className="bg-green-700 text-white text-xs md:text-base">Expense Name</th>
+                        <th className="bg-green-700 text-white text-xs md:text-base">Category</th>
+                        <th className="bg-green-700 text-white text-xs md:text-base">Amount</th>
                     </tr>
                 </thead>
                 <tbody>
                     {dataState && dataState.map((item: RootState, index: number) => (
                         <tr key={index} className="border-solid border-2 border-slate-400">
-                            <td className="text-gray-800">{item.title}</td>
-                            <td className="text-gray-800">{item.category}</td>
-                            <td className="text-gray-800">$ {item.amount}</td>
+                            <td className="text-gray-800 text-xs md:text-base">{item.title}</td>
+                            <td className="text-gray-800 text-xs md:text-base">{item.category}</td>
+                            <td className="text-gray-800 text-xs md:text-base">$ {item.amount}</td>
                         </tr>
                     ))}
                     <tr className="border-solid border-2 border-slate-400">
                         <td></td>
-                        <td className="font-bold bg-green-700 text-white">Total</td>
-                        <td className="font-bold bg-green-700 text-white">$ {totalAmount()}</td>
+                        <td className="font-bold bg-green-700 text-white text-lg">Total</td>
+                        <td className="font-bold bg-green-700 text-white text-lg">$ {totalAmount()}</td>
                     </tr>
                 </tbody>
             </table>
