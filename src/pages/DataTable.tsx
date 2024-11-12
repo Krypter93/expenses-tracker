6c4623cfb6794748de39996792c61ff341081189
Ahmed <ahmedfdez93@gmail.com>
2024-11-12 05:13:20 -0500

    Add dark mode to DataTable component

--- src/pages/DataTable.tsx
+++ src/pages/DataTable.tsx
@@ -1,33 +1,34 @@
 import { RootState } from "../redux/store"
 import { totalAmount } from "./service/totalAmount";
+import { useSelector, UseSelector } from "react-redux";
 
 export const DataTable = () => {
     const dataState = JSON.parse(localStorage.getItem('expenses') || '[]')
-    console.log(dataState);
+    const darkModeState = useSelector((state: RootState) => state.darkMode.value)
     
     return (
         <>
-        <section className="w-4/2 md:w-4/5 bg-gray-200 mt-16 mx-auto font-mono flex justify-between rounded">
+        <section className={`w-4/2 md:w-4/5 mt-16 mx-auto font-mono flex justify-between rounded ${darkModeState ? 'bg-gray-700' : 'bg-gray-200'}`}>
         <table className="w-full text-center border-solid border-2 border-slate-400">
                 <thead>
                     <tr>
-                        <th className="bg-green-700 text-white text-xs md:text-base">Expense Name</th>
-                        <th className="bg-green-700 text-white text-xs md:text-base">Category</th>
-                        <th className="bg-green-700 text-white text-xs md:text-base">Amount</th>
+                        <th className={` text-white text-xs md:text-xl ${darkModeState ? 'bg-slate-900' : 'bg-green-700'}`}>Expense Name</th>
+                        <th className={` text-white text-xs md:text-xl ${darkModeState ? 'bg-slate-900' : 'bg-green-700'}`}>Category</th>
+                        <th className={` text-white text-xs md:text-xl ${darkModeState ? 'bg-slate-900' : 'bg-green-700'}`}>Amount</th>
                     </tr>
                 </thead>
                 <tbody>
                     {dataState && dataState.map((item: RootState, index: number) => (
                         <tr key={index} className="border-solid border-2 border-slate-400">
-                            <td className="text-gray-800 text-xs md:text-base">{item.title}</td>
-                            <td className="text-gray-800 text-xs md:text-base">{item.category}</td>
-                            <td className="text-gray-800 text-xs md:text-base">$ {item.amount}</td>
+                            <td className={`text-xs md:text-base ${darkModeState ? 'text-white' : 'text-gray-800'}`}>{item.title}</td>
+                            <td className={`text-xs md:text-base ${darkModeState ? 'text-white' : 'text-gray-800'}`}>{item.category}</td>
+                            <td className={`text-xs md:text-base ${darkModeState ? 'text-white' : 'text-gray-800'}`}>$ {item.amount}</td>
                         </tr>
                     ))}
                     <tr className="border-solid border-2 border-slate-400">
                         <td></td>
-                        <td className="font-bold bg-green-700 text-white text-lg">Total</td>
-                        <td className="font-bold bg-green-700 text-white text-lg">$ {totalAmount()}</td>
+                        <td className={`font-bold  text-white text-xl ${darkModeState ? 'bg-slate-900' : 'bg-green-700'}`}>Total</td>
+                        <td className={`font-bold  text-white text-xl ${darkModeState ? 'bg-slate-900' : 'bg-green-700'}`}>$ {totalAmount()}</td>
                     </tr>
                 </tbody>
             </table>
